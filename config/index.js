f163c8b61f91a8bf9a5ab97b889fd95101581758
Bonito <miguelvalden77@gmail.com>
2022-09-28 08:59:55 +0200

    vercel

--- config/index.js
+++ config/index.js
@@ -20,12 +20,12 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  // app.use(
-  //   cors({
-  //     credentials: true,
-  //     origin: process.env.ORIGIN || "http://localhost:5173",
-  //   })
-  // );
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN || "http://localhost:5173",
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
